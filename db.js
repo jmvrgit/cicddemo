a51159ef6dfd95cd13935d02d7d73d1783d7b878
Jon Marco Reyes <rjonmarco@gmail.com>
2022-04-27 00:40:50 +0800

    Added MySQL name to Dockerfile

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
-var mysql = require('mysql'); 
+var mysql = require('mysql2'); 
 
 var connection = mysql.createConnection({
-host: '192.168.254.104',
-user: 'root',
-password: '',
-database: 'seccam_db',
+    host: '192.168.254.104',
+    port: 9002,
+    user: 'root',
+    password: 'testDBpassword',
+    database: 'seccam_db',
 });
 
 
